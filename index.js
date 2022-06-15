627c02b5f3ca35da18be546ddcc891b00dc9fe35
qJay44 <m70155302@yandex.ru>
2022-06-15 18:44:18 +0300

    Player sprite - jump

--- index.js
+++ index.js
@@ -52,6 +52,14 @@ const player = new Fighter({
         run: {
             imageSrc: './img/samuraiMack/Run.png',
             framesMax: 8,
+        },
+        jump: {
+            imageSrc: './img/samuraiMack/Jump.png',
+            framesMax: 2,
+        },
+        fall: {
+            imageSrc: './img/samuraiMack/Fall.png',
+            framesMax: 2,
         }
     }
 });
@@ -104,13 +112,20 @@ function animate() {
     enemy.velocity.x = 0;
 
     // player movement
-    player.image = player.sprites.idle.image;
     if (keys.a.pressed && player.lastKey == 'a') {
         player.velocity.x = -5;
-        player.image = player.sprites.run.image;
+        player.switchSprite('run');
     } else if (keys.d.pressed && player.lastKey == 'd') {
         player.velocity.x = 5;
-        player.image = player.sprites.run.image;
+        player.switchSprite('run');
+    } else {
+        player.switchSprite('idle');
+    }
+
+    if (player.velocity.y < 0) {
+        player.switchSprite('jump');
+    } else if (player.velocity.y > 0) {
+        player.switchSprite('fall');
     }
 
     // enemy movement
--- js/classes.js
+++ js/classes.js
@@ -103,8 +103,45 @@ class Fighter extends Sprite {
         this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
 
+        // gravity
         if (this.position.y + this.height + this.velocity.y >= h - 95) {
             this.velocity.y = 0;
+            this.position.y = 475;
         } else this.velocity.y += Fighter.gravity;
+    }
+
+    switchSprite(sprite) {
+        switch (sprite) {
+            case 'idle':
+                if (this.image != this.sprites.idle.image) {
+                    this.image = this.sprites.idle.image;
+                    this.framesMax = this.sprites.idle.framesMax;
+                    this.framesCurrent = 0;
+                }
+                break;
+
+            case 'run':
+                if (this.image != this.sprites.run.image) {
+                    this.image = this.sprites.run.image;
+                    this.framesMax = this.sprites.run.framesMax;
+                    this.framesCurrent = 0;
+                }
+                break;
+
+            case 'jump':
+                if (this.image != this.sprites.jump.image) {
+                    this.image = this.sprites.jump.image;
+                    this.framesMax = this.sprites.jump.framesMax;
+                    this.framesCurrent = 0;
+                }
+                break;
+            case 'fall':
+                if (this.image != this.sprites.fall.image) {
+                    this.image = this.sprites.fall.image;
+                    this.framesMax = this.sprites.fall.framesMax;
+                    this.framesCurrent = 0;
+                }
+                break;
+        }
     }
 }
